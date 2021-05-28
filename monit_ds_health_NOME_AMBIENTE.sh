e71b71fd3590b307773a223e4916cbf484fda873
lucasrodriguesdelira <54848684+lucasrodriguesdelira@users.noreply.github.com>
2021-05-28 13:36:47 -0300

    Update monit_ds_health_NOME_AMBIENTE.sh

--- monit_ds_health_NOME_AMBIENTE.sh
+++ monit_ds_health_NOME_AMBIENTE.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+parm=$*
+
+d_dominio=/u01/oracle/Middleware/Oracle_Home/wlserver/server
+d_scripts=/u01/app/monitoracao/dir_exec_py
+
+# Configurar variaveis de ambiente
+. ${d_dominio}/bin/setWLSEnv.sh
+
+filename=`basename $0`
+scriptname=${filename%.*}
+
+# Chama wlst e python
+java weblogic.WLST ${d_scripts}/${scriptname}.py ${parm}
