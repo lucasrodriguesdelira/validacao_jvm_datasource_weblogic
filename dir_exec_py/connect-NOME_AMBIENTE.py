1d2bca2003150450734d41d6720e7e4ada8dd95c
lucasrodriguesdelira <54848684+lucasrodriguesdelira@users.noreply.github.com>
2021-05-28 13:42:50 -0300

    Update connect-NOME_AMBIENTE.py

--- dir_exec_py/connect-NOME_AMBIENTE.py
+++ dir_exec_py/connect-NOME_AMBIENTE.py
@@ -1,1 +1,12 @@
+import sys.argv
 
+# VIA ARQUIVO COM CRIPTOGRAFIA
+#configFile='/u01/app/scripts/security/NOME_DO_AMBIENTE-config-file'
+#keyFile='/app/scripts/security/NOME_DO_AMBIENTE-key-file'
+
+#USER SENHA DIRETO
+try:
+    connect('USER','SENHA',url='t3://10.0.0.0:7001')
+
+except Exception, e:
+    print(str(e))
